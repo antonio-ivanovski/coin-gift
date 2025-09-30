23ce5785c9842f64f654c694b0959e8cbb4b831b
Antonio Ivanovski <antonio-ivanovski@users.noreply.github.com>
2025-09-30 21:44:46 +0200

    testing ln

---
+++ server/src/testing.ts
@@ -0,0 +1,9 @@
+// import "websocket-polyfill";
+import { NWCClient } from "@getalby/sdk/nwc";
+
+const nwcConnectionCode = "<YOUR_NWC_CONNECTION_CODE_HERE>";
+
+const nwcClient = new NWCClient({ nostrWalletConnectUrl: nwcConnectionCode });
+nwcClient.makeHoldInvoice;
+const invoice = await nwcClient.makeInvoice({ amount: 1 });
+console.log("Invoice:", invoice);
